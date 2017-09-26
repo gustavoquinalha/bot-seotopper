924c2548835de52895f98f8b14a8ee13cb188dd2
gustavoquinalha <gustavo_quinalha@hotmail.com>
2017-09-26 15:14:23 -0300

    first commit

--- components/menutop.vue
+++ components/menutop.vue
@@ -1,25 +1,25 @@
 <template>
-<div class="">
-  menu
-</div>
+  <div class="">
+    <!-- <nuxt-link to="/">Home</nuxt-link>
+    <nuxt-link to="/start">Start</nuxt-link> -->
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'topo',
-    data() {
-      return {
-        showMenu: false
-      }
-    },
-    methods: {
-      toggleShowMenu() {
-        this.showMenu = !this.showMenu
-      }
+export default {
+  name: 'topo',
+  data() {
+    return {
+      showMenu: false
+    }
+  },
+  methods: {
+    toggleShowMenu() {
+      this.showMenu = !this.showMenu
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
