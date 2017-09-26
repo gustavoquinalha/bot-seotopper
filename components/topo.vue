3c8a9bdfb8b9b41ebe4ce465fc5d63cf12fce12b
gustavoquinalha <gustavo_quinalha@hotmail.com>
2017-09-26 14:30:33 -0300

    first commit

---
+++ components/topo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <header>
+        <div class="size container justify-content-center align-items-center">
+          <div class="">
+            teste
+          </div>
+        </div>
+    </header>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'topo',
+    data() {
+      return {
+
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
