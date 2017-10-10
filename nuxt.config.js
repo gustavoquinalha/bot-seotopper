70195e189023ea8159e1224feabffd728d484cc9
gustavoquinalha <gustavo_quinalha@hotmail.com>
2017-10-10 13:59:29 -0300

    toppppp

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,38 +36,36 @@ module.exports = {
       { name: 'twitter:image', content: 'https://gustavoquinalha.github.io/seotopper/img/intro.png' },
       // android
       { name: 'msapplication-TileColor', content: '#0081FF' },
-      { name: 'msapplication-TileImage', content: '/ms-icon-144x144.png' },
+      { name: 'msapplication-TileImage', content: './ms-icon-144x144.png' },
       { name: 'theme-color', content: '#0081FF' }
     ],
     link: [
       { rel: 'sitemap', type: 'application/xml', content: 'https://gustavoquinalha.github.io/seotopper/sitemap.xml' },
-      { rel: 'manifest', href: '/manifest.json' },
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'apple-touch-icon', sizes: '57x57', href: '/icons/apple-icon-57x57.png' },
-      { rel: 'apple-touch-icon', sizes: '60x60', href: '/icons/apple-icon-60x60.png' },
-      { rel: 'apple-touch-icon', sizes: '72x72', href: '/icons/apple-icon-72x72.png' },
-      { rel: 'apple-touch-icon', sizes: '76x76', href: '/icons/apple-icon-76x76.png' },
-      { rel: 'apple-touch-icon', sizes: '114x114', href: '/icons/apple-icon-114x114.png' },
-      { rel: 'apple-touch-icon', sizes: '120x120', href: '/icons/apple-icon-120x120.png' },
-      { rel: 'apple-touch-icon', sizes: '144x144', href: '/icons/apple-icon-144x144.png' },
-      { rel: 'apple-touch-icon', sizes: '152x152', href: '/icons/apple-icon-152x152.png' },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/apple-icon-180x180.png' },
-      { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/icons/android-icon-192x192.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icons/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/icons/favicon-96x96.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' },
+      { rel: 'manifest', href: './manifest.json' },
+      { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '57x57', href: './icons/apple-icon-57x57.png' },
+      { rel: 'apple-touch-icon', sizes: '60x60', href: './icons/apple-icon-60x60.png' },
+      { rel: 'apple-touch-icon', sizes: '72x72', href: './icons/apple-icon-72x72.png' },
+      { rel: 'apple-touch-icon', sizes: '76x76', href: './icons/apple-icon-76x76.png' },
+      { rel: 'apple-touch-icon', sizes: '114x114', href: './icons/apple-icon-114x114.png' },
+      { rel: 'apple-touch-icon', sizes: '120x120', href: './icons/apple-icon-120x120.png' },
+      { rel: 'apple-touch-icon', sizes: '144x144', href: './icons/apple-icon-144x144.png' },
+      { rel: 'apple-touch-icon', sizes: '152x152', href: './icons/apple-icon-152x152.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: './icons/apple-icon-180x180.png' },
+      { rel: 'icon', type: 'image/png', sizes: '192x192', href: './icons/android-icon-192x192.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: './icons/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '96x96', href: './icons/favicon-96x96.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: './icons/favicon-16x16.png' },
       { rel: 'base', href: 'https://gustavoquinalha.github.io/seotopper/' },
       { rel: 'canonical', href: 'https://gustavoquinalha.github.io/seotopper/' }
     ],
     script: [{
-      // src: 'https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js'
     }]
   },
 
   loading: { color: '#00ce8c', height: '5px' },
 
   plugins: [
-  // { src: '~plugins/wowjs.js', ssr: false }
   ],
 
   build: {
